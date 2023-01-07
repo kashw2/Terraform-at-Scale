6f91a1b4c2018dc61e50b76b8756645cb85a688c
kashw2 <supra4keanu@hotmail.com>
2023-01-07 17:06:56 +1000

    feat(frontdoor-aks-nginx): added bootstrap

---
+++ frontdoor-aks-nginx/provider.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.38.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.31.0"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = "stfdaksnginxtf"
+    resource_group_name  = "rg-terraform"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "506fa660-c543-49a7-b766-aca51cd53ac4"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    application_insights {
+      disable_generated_rule = false
+    }
+    log_analytics_workspace {
+      permanently_delete_on_destroy = true
+    }
+  }
+}
