bf03909a263d91f987d8619bf78d2dcef102e4bc
kashw2 <supra4keanu@hotmail.com>
2023-01-08 02:57:00 +1000

    feat(frontdoor-aks-nginx): added kubernetes ingress

---
+++ frontdoor-aks-nginx/modules/kubernetes/ingress/ingress.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_ingress_v1" "ingress" {
+  metadata {
+    name = "ingress"
+    namespace = var.namespace
+    annotations = {
+      "kubernetes.io/ingress.class"           = "nginx"
+      "nginx.ingress.kubernetes.io/use-regex" = true
+    }
+  }
+  spec {
+    default_backend {
+      service {
+        name = "nginx"
+        port {
+          number = 80
+        }
+      }
+    }
+  }
+}
