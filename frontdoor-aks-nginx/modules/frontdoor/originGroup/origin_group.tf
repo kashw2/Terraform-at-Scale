290917f54754f312ce31e315247646be7a6b88a6
kashw2 <supra4keanu@hotmail.com>
2023-01-08 12:01:01 +1000

    feat(frontdoor-aks-nginx): added frontdoor origin group

---
+++ frontdoor-aks-nginx/modules/frontdoor/originGroup/origin_group.tf
@@ -0,0 +1,41 @@
+resource "azurerm_cdn_frontdoor_origin_group" "app_1" {
+  name                     = "app1"
+  cdn_frontdoor_profile_id = var.profile_id
+  session_affinity_enabled = true
+
+  restore_traffic_time_to_healed_or_new_endpoint_in_minutes = 10
+
+  health_probe {
+    interval_in_seconds = 240
+    path                = "/"
+    protocol            = "Https"
+    request_type        = "HEAD"
+  }
+
+  load_balancing {
+    additional_latency_in_milliseconds = 0
+    sample_size                        = 16
+    successful_samples_required        = 3
+  }
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "app_2" {
+  name                     = "app2"
+  cdn_frontdoor_profile_id = var.profile_id
+  session_affinity_enabled = true
+
+  restore_traffic_time_to_healed_or_new_endpoint_in_minutes = 10
+
+  health_probe {
+    interval_in_seconds = 240
+    path                = "/"
+    protocol            = "Https"
+    request_type        = "HEAD"
+  }
+
+  load_balancing {
+    additional_latency_in_milliseconds = 0
+    sample_size                        = 16
+    successful_samples_required        = 3
+  }
+}
