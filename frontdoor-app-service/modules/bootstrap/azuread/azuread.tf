417b7c933e3e08ab6d14402ef12df043105e3022
kashw2 <supra4keanu@hotmail.com>
2023-01-28 12:59:47 +1000

    feat(frontdoor-app-service): added bootstrap module

---
+++ frontdoor-app-service/modules/bootstrap/azuread/azuread.tf
@@ -0,0 +1,26 @@
+resource "azuread_application" "terraform" {
+  display_name = "Terraform (frontdoor-app-service) (Terraform at Scale)"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
+
+resource "azuread_service_principal" "terraform" {
+  application_id = azuread_application.terraform.application_id
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
+
+resource "azuread_service_principal_password" "terraform" {
+  service_principal_id = azuread_service_principal.terraform.id
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+}
---
+++ frontdoor-app-service/provider.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_version = "1.3.7"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.41.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.33.0"
+    }
+  }
+  backend "azurerm" {
+    storage_account_name = "stfdappservicetf"
+    resource_group_name  = "rg-terraform"
+    container_name       = "terraform"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "506fa660-c543-49a7-b766-aca51cd53ac4"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    application_insights {
+      disable_generated_rule = false
+    }
+    log_analytics_workspace {
+      permanently_delete_on_destroy = true
+    }
+  }
+}
